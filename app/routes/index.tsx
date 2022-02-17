7a20cfe67ea53ce158351ac2391eeac0db6ad498
garebareDA <garedaiki@gmail.com>
2022-02-18 04:25:04 +0900

    add:Base Number Card

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,23 @@
-import { Grid, Card, Text } from "@nextui-org/react";
+import { Grid, Card, Text, Row } from "@nextui-org/react";
 
-import baseNumberIcon from "../../assets/image/icon/base-number.png";
+import baseNumberIcon from "../assets/image/icon/NumberBase.png";
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <Grid.Container>
-        <Grid xs={6} sm={3}>
-          <Card>
+      <Grid.Container justify="center" gap={2}>
+        <Grid xs={5} sm={1.5}>
+          <Card clickable hoverable cover>
             <Card.Body>
-              <Text></Text>
+              <Card.Image css={{filter:"invert(100%)"}} src={baseNumberIcon} width={"80%"} height="140px"/>
             </Card.Body>
+            <Card.Footer >
+              <Row justify="center">
+                <Text b>
+                  Base Number
+                </Text>
+              </Row>
+            </Card.Footer>
           </Card>
         </Grid>
       </Grid.Container>

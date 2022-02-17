892de7cbf3e2190d64ca461c9259e80bbae48d9f
garebareDA <garedaiki@gmail.com>
2022-02-17 23:15:39 +0900

    add:NumberBase icon

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,19 @@
+import { Grid, Card, Text } from "@nextui-org/react";
+
+import baseNumberIcon from "../../assets/image/icon/base-number.png";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Hello</h1>
+      <Grid.Container>
+        <Grid xs={6} sm={3}>
+          <Card>
+            <Card.Body>
+              <Text></Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </div>
   );
 }

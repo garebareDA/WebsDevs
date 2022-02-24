468f1bb514fb93228675252008485def3cd51c14
garebareDA <garedaiki@gmail.com>
2022-02-24 22:38:32 +0900

    add:textDiff

---
+++ app/hooks/textDiff.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export const useTextDiff = ():{
+  oldText:string,
+  newText:string,
+  setOldText: (text: string) => void,
+  setNewText: (text: string) => void,
+} => {
+  const [oldText, setOldText] = useState<string>("");
+  const [newText, setNewText] = useState<string>("");
+
+  return {
+    oldText,
+    setOldText,
+    newText,
+    setNewText,
+  };
+};

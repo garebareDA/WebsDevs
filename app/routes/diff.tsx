b79cdafa8a0c840549050da76b1f61996b6d2b97
garebareDA <garedaiki@gmail.com>
2022-02-25 18:38:49 +0900

    fix:diff

--- app/routes/diff.tsx
+++ app/routes/diff.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Container, Spacer, Textarea } from "@nextui-org/react";
+import { Container, Spacer, Textarea, Grid } from "@nextui-org/react";
 import { Title } from "~/components/util/title";
 import ReactDiffViewer from "ab-react-diff-viewer";
 
-export default function Index():React.ReactElement{
+export default function Index(): React.ReactElement {
   const [newText, setNewDiff] = useState<string>('');
   const [oldText, setOldDiff] = useState<string>('');
 
@@ -13,13 +13,22 @@ export default function Index():React.ReactElement{
         <Spacer y={1} />
         <Title title="Text Diff" />
         <Spacer y={2} />
-        <Textarea onChange={(e) => {
-          setOldDiff(e.target.value);
-        }}></Textarea>
-        <Textarea onChange={(e) => {
-          setNewDiff(e.target.value);
-        }}></Textarea>
-        <ReactDiffViewer oldValue={oldText} newValue={newText} splitView={true}/>
+        <Grid.Container gap={1}>
+          <Grid xs={6} alignItems="flex-start">
+            <Textarea labelPlaceholder="old text" width="100%" rows={10} onChange={(e) => {
+              setOldDiff(e.target.value);
+            }}></Textarea>
+          </Grid>
+          <Grid xs={6}>
+            <Textarea labelPlaceholder="new text" width="100%" rows={10} onChange={(e) => {
+              setNewDiff(e.target.value);
+            }}></Textarea>
+          </Grid>
+          <Spacer y={1} />
+          <Grid xs={12}>
+            <ReactDiffViewer oldValue={oldText} newValue={newText} splitView={true} />
+          </Grid>
+        </Grid.Container>
       </Container>
     </div>
   );

f523ec09f704f353bc6147ef040d34a8629b04e6
garebareDA <garedaiki@gmail.com>
2022-02-26 18:12:58 +0900

    add:color blind

---
+++ app/hooks/colorBlind.ts
@@ -0,0 +1,7 @@
+import { simulate } from '@bjornlu/colorblind';
+import getColors from 'get-image-colors';
+const changeColor = require('change-image-color');
+
+export const useColorBlind = () => {
+  return;
+};

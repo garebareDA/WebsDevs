ff9d27e76f359f0da8df25df2e22f7200ff8a1bf
garebareDA <garedaiki@gmail.com>
2022-02-28 01:15:13 +0900

    wip:color blind

--- app/routes/colorBlind.tsx
+++ app/routes/colorBlind.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Spacer, Row } from "@nextui-org/react";
+import { Container, Spacer, Row, Card, Grid, Text } from "@nextui-org/react";
 import { Title } from "~/components/util/title";
 import { FileUploader } from "react-drag-drop-files";
 import { useColorBlind } from "~/hooks/colorBlind";
@@ -17,10 +17,61 @@ export default function Index(): React.ReactElement {
             setFile(file);
           }}></FileUploader>
         </Row>
-        <img src={original} alt="" />
-        <img src={protanopia} alt="" />
-        <img src={deuteranopia} alt="" />
-        <img src={tritanopia} alt="" />
+        <Spacer y={2} />
+        <Grid.Container gap={1}>
+          <Grid xs={6}>
+            <Card>
+              <Card.Body>
+                <img src={original} alt="" />
+              </Card.Body>
+              <Card.Footer>
+                <Text>
+                  Original
+                </Text>
+              </Card.Footer>
+            </Card>
+          </Grid>
+
+          <Grid xs={6}>
+            <Card>
+              <Card.Body>
+                <img src={protanopia} alt="" />
+              </Card.Body>
+              <Card.Footer>
+                <Text>
+                  Protanopia
+                </Text>
+              </Card.Footer>
+            </Card>
+          </Grid>
+
+          <Grid xs={6}>
+            <Card>
+              <Card.Body>
+                <img src={deuteranopia} alt="" />
+              </Card.Body>
+              <Card.Footer>
+                <Text>
+                  Deuteranopia
+                </Text>
+              </Card.Footer>
+            </Card>
+          </Grid>
+
+          <Grid xs={6}>
+            <Card>
+              <Card.Body>
+                <img src={tritanopia} alt="" />
+              </Card.Body>
+              <Card.Footer>
+                <Text>
+                  Tritanopia
+                </Text>
+              </Card.Footer>
+            </Card>
+          </Grid>
+
+        </Grid.Container>
       </Container>
     </div>
   );

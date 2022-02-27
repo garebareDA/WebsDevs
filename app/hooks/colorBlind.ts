f697ccdabe846928d3e9c20d274fe7dedd2cca9d
garebareDA <garedaiki@gmail.com>
2022-02-27 13:32:58 +0900

    wip

--- app/hooks/colorBlind.ts
+++ app/hooks/colorBlind.ts
@@ -17,11 +17,26 @@ export const useColorBlind = (): {
         const ctx = canvas.getContext('2d');
         ctx.drawImage(image, 0, 0);
         const imageData = ctx.getImageData(0, 0, image.width, image.height);
+        for(let y = 0; y < imageData.height; y++) {
+          for(let x=0; x<imageData.width; x++) {
+            const index = (y * imageData.width + x) * 4;
+            const r = imageData.data[index];
+            const g = imageData.data[index + 1];
+            const b = imageData.data[index + 2];
+
+            const { r: r2, g: g2, b: b2 } = simulate({ r, g, b }, "protanopia");
+            imageData.data[index] = r2;
+            imageData.data[index + 1] = g2;
+            imageData.data[index + 2] = b2;
+          }
+        }
+        ctx.putImageData(imageData, 0, 0);
+        const url = canvas.toDataURL();
+        console.log(url);
       };
       image.src = e.target?.result as string;
     };
     reader.readAsDataURL(file);
-
   }, [file]);
 
   return {

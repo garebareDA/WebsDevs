1e3a8ff4bec1ea81672e7cc6208646df35ef3396
garebareDA <garedaiki@gmail.com>
2022-03-03 05:39:51 +0900

    fix:decodeEncode ant jwt hook

--- app/components/decodeEncode.tsx
+++ app/components/decodeEncode.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Textarea, Grid } from "@nextui-org/react";
+import { Textarea, Grid, Spacer, Button } from "@nextui-org/react";
 
 type Props = {
   encodeSet: (value: string) => void;
@@ -25,35 +25,69 @@ export const TextAreaEncodeDecode = ({
   readonly = false,
 }: Props): React.ReactElement => {
   return (
-    <Grid.Container gap={2} justify="center" alignItems="flex-start">
-      <Grid xs={6}>
-        <Textarea
-          maxRows={500}
-          minRows={20}
-          labelPlaceholder={decodeLabel}
-          fullWidth
-          onChange={(e) => {
-            decodeSet(e.target.value);
-          }}
-          value={decodeValue}
-          status={decodeStatus}
-        />
-      </Grid>
+    <div>
 
-      <Grid xs={6}>
-        <Textarea
-          readOnly={readonly}
-          maxRows={500}
-          minRows={20}
-          labelPlaceholder={encodeLabel}
-          fullWidth
-          onChange={(e) => {
-            encodeSet(e.target.value);
-          }}
-          value={encodeValue}
-          status={encodeStatus}
-        />
-      </Grid>
-    </Grid.Container>
+      <Grid.Container gap={2} justify="center" alignItems="flex-start">
+        <Grid xs={6}>
+          <Textarea
+            maxRows={500}
+            minRows={20}
+            labelPlaceholder={decodeLabel}
+            fullWidth
+            onChange={(e) => {
+              decodeSet(e.target.value);
+            }}
+            value={decodeValue}
+            status={decodeStatus}
+          />
+        </Grid>
+
+        <Grid xs={6}>
+          <Textarea
+            readOnly={readonly}
+            maxRows={500}
+            minRows={20}
+            labelPlaceholder={encodeLabel}
+            fullWidth
+            onChange={(e) => {
+              encodeSet(e.target.value);
+            }}
+            value={encodeValue}
+            status={encodeStatus}
+          />
+        </Grid>
+      </Grid.Container>
+      <Grid.Container gap={1}>
+        <Grid xs={6} justify="flex-end">
+          <Button.Group>
+            <Button onClick={() => {
+              navigator.clipboard.writeText(decodeValue);
+            }}> Copy </Button>
+            <Button onClick={() => {
+              navigator.clipboard.readText().then((text) => {
+                decodeSet(text);
+              });
+            }}>Pest</Button>
+            <Button onClick={() => {
+              decodeSet("");
+            }}> Delete </Button>
+          </Button.Group>
+        </Grid>
+
+        <Grid xs={6} justify="flex-end">
+          <Button.Group>
+            <Button onClick={() => {
+              navigator.clipboard.writeText(encodeValue);
+            }}> Copy </Button>
+            {!readonly && <Button onClick={() => {
+              navigator.clipboard.readText().then((text) => {
+                encodeSet(text);
+              });
+            }}>Pest</Button>}
+            {!readonly && <Button onClick={() => { encodeSet(""); }}> Delete </Button>}
+          </Button.Group>
+        </Grid>
+      </Grid.Container>
+    </div>
   );
 };

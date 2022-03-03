9dec88f7eb183bca0eb43f6af7516bcd94003269
garebareDA <garedaiki@gmail.com>
2022-03-03 16:57:53 +0900

    fixLhash

--- app/components/generateInput.tsx
+++ app/components/generateInput.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Textarea } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
 
 type Props = {
   name: string;
@@ -8,6 +8,6 @@ type Props = {
 
 export const GenerateInput: React.VFC<Props> = ({ name, value }: Props) => {
   return (
-    <Textarea labelPlaceholder={name} width="100%" value={value} readOnly rows={10} />
+    <Input labelPlaceholder={name} width="100%" value={value} readOnly rows={10} />
   );
 };
